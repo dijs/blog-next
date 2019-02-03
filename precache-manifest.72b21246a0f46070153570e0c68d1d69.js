0d099bb802add14f8954519a247cd5a18151a971
Richard van der Dys <richard.vanderdys@gmail.com>
2019-02-03 19:07:28 +0200

    Updates

---
+++ precache-manifest.72b21246a0f46070153570e0c68d1d69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "28700b9e60e334850e2547ec22da09c6",
+    "url": "/blog-next/static/media/npm.28700b9e.svg"
+  },
+  {
+    "revision": "619231ff8ec7a6b4d39aede30fa9890e",
+    "url": "/blog-next/static/media/linkedin.619231ff.svg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/blog-next/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "e97293d0d12b3f4c86d820fd2ba58d7c",
+    "url": "/blog-next/static/media/avatar.e97293d0.svg"
+  },
+  {
+    "revision": "eaa506c30f9689d0aa0a",
+    "url": "/blog-next/static/js/runtime~main.eaa506c3.js"
+  },
+  {
+    "revision": "311fa57332c4c861aea3",
+    "url": "/blog-next/static/js/main.311fa573.chunk.js"
+  },
+  {
+    "revision": "620ffcd682cf12b57ef0",
+    "url": "/blog-next/static/js/1.620ffcd6.chunk.js"
+  },
+  {
+    "revision": "311fa57332c4c861aea3",
+    "url": "/blog-next/static/css/main.ef8749ab.chunk.css"
+  },
+  {
+    "revision": "70e4074280e9353a7537990b6e6509fb",
+    "url": "/blog-next/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.468cd206f35124ac6fc76082d94201f0.js"
+  "/blog-next/precache-manifest.72b21246a0f46070153570e0c68d1d69.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/blog-next/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
