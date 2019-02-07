68a4a0ce73591c701054f770b80baf2e70677d43
Richard van der Dys <richard.vanderdys@gmail.com>
2019-02-07 17:23:58 +0200

    Use some hooks

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 import { ReactComponent as Head } from '../images/avatar.svg';
 
 export default function Avatar() {
+	const [active, setActive] = useState(false);
+	function activate() {
+		if (active) return;
+		setActive(true);
+		setTimeout(() => setActive(false), 3000);
+	}
 	return (
-		<div className="avatar">
+		<div className={classnames('avatar', { active })}>
 			<div className="note-wrapper n1">
 				<div className="note">♫</div>
 			</div>
@@ -16,7 +23,7 @@ export default function Avatar() {
 			<div className="note-wrapper n4">
 				<div className="note">♪</div>
 			</div>
-			<Head />
+			<Head onClick={activate} onMouseOver={activate} />
 		</div>
 	);
 }
