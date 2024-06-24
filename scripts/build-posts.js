3783cd4cb3757ecb1b66954b62e2ec1ae8d86736
richardvanderdys <richard.vanderdys@smarterdx.com>
2024-06-24 19:52:37 +0300

    different posts dir?

--- scripts/build-posts.js
+++ scripts/build-posts.js
@@ -3,7 +3,7 @@ import path from 'path';
 import metadataParser from 'parse-md';
 import dateFns from 'date-fns';
 
-const slug = text =>
+const slug = (text) =>
   text
     .toLowerCase()
     .replace(/\//, '-')
@@ -12,22 +12,25 @@ const slug = text =>
     .replace(/^-+|-+$/g, ''); // remove leading, trailing -
 
 export default function buildPosts() {
-  return fs
-    .readdirSync('./posts')
-    .map(name => fs.readFileSync(path.join('./posts', name), 'utf8'))
-    .map(source => metadataParser(source))
-    .filter(({ metadata: { published } }) => published)
-    .map(post => {
-      post.metadata.date = new Date(post.metadata.date);
-      return post;
-    })
-    .sort((a, b) => +b.metadata.date - +a.metadata.date)
-    .map((post, index) => {
-      if (!post.metadata.blurb) post.metadata.blurb = 'Write something here!';
-      post.metadata.date = dateFns.format(post.metadata.date, 'MMM D, YYYY');
-      post.slug = slug(post.metadata.title);
-      post.index = index;
-      post.path = `/post/${post.slug}`;
-      return post;
-    });
+  return (
+    fs
+      // use a path vercel would understand
+      .readdirSync(path.join('./posts'))
+      .map((name) => fs.readFileSync(path.join('./posts', name), 'utf8'))
+      .map((source) => metadataParser(source))
+      .filter(({ metadata: { published } }) => published)
+      .map((post) => {
+        post.metadata.date = new Date(post.metadata.date);
+        return post;
+      })
+      .sort((a, b) => +b.metadata.date - +a.metadata.date)
+      .map((post, index) => {
+        if (!post.metadata.blurb) post.metadata.blurb = 'Write something here!';
+        post.metadata.date = dateFns.format(post.metadata.date, 'MMM D, YYYY');
+        post.slug = slug(post.metadata.title);
+        post.index = index;
+        post.path = `/post/${post.slug}`;
+        return post;
+      })
+  );
 }
